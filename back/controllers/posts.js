e3020b266927f771bb102c07485e057fb5eb6f25
nicolascastagna <100592012+nicolascastagna@users.noreply.github.com>
2022-07-05 13:55:34 +0200

    fix modify and delete post user

--- back/controllers/posts.js
+++ back/controllers/posts.js
@@ -6,6 +6,7 @@ exports.createPost = (req, res, next) => {
 
   const post = new Posts({
     ...postObject,
+    UserId: req.auth.userId,
     postFile: `${req.protocol}://${req.get("host")}/images/${req.file}`,
   });
   post
@@ -15,66 +16,70 @@ exports.createPost = (req, res, next) => {
 };
 
 exports.modifyPost = (req, res, next) => {
-  if (req.params.id || req.auth.admin) {
-    const postObject = req.file
-      ? {
-          ...req.body,
-          postFile: `${req.protocol}://${req.get("host")}/images/${
-            req.file.filename
-          }`,
-        }
-      : { ...req.body };
-    Posts.findOne({ where: { id: req.params.id } }).then((post) => {
-      if (!req.file || !filename) {
-        Posts.update(
-          { ...postObject, id: req.params.id },
-          { where: { id: req.params.id } }
-        )
-          .then(() => res.status(200).json({ message: "Post modifié !" }))
-          .catch((error) => res.status(400).json({ error }));
-      } else {
-        const filename = post.postFile.split("/images/")[1];
-        fs.unlink(`images/${filename}`, () => {
+  const admin = req.auth.admin === req.auth.userId;
+  Posts.findOne({ where: { id: req.params.id } }).then((post) => {
+    if (post.UserId == req.auth.userId || admin) {
+      const postObject = req.file
+        ? {
+            ...req.body,
+            postFile: `${req.protocol}://${req.get("host")}/images/${
+              req.file.filename
+            }`,
+          }
+        : { ...req.body };
+      Posts.findOne({ where: { id: req.params.id } }).then((post) => {
+        if (!req.file || !filename) {
           Posts.update(
             { ...postObject, id: req.params.id },
             { where: { id: req.params.id } }
           )
             .then(() => res.status(200).json({ message: "Post modifié !" }))
             .catch((error) => res.status(400).json({ error }));
-        });
-      }
-    });
-  } else {
-    return res.status(401).json({
-      error: new error("Requête non autorisée !"),
-    });
-  }
+        } else {
+          const filename = post.postFile.split("/images/")[1];
+          fs.unlink(`images/${filename}`, () => {
+            Posts.update(
+              { ...postObject, id: req.params.id },
+              { where: { id: req.params.id } }
+            )
+              .then(() => res.status(200).json({ message: "Post modifié !" }))
+              .catch((error) => res.status(400).json({ error }));
+          });
+        }
+      });
+    } else {
+      return res.status(401).json({
+        error: new error("Requête non autorisée !"),
+      });
+    }
+  });
 };
 
 exports.deletePost = (req, res, next) => {
-  Posts.findOne({ where: { id: req.params.id } })
-    .then((post) => {
-      if (!post) {
-        return res.status(404).json({
-          error: new error("Post non trouvé !"),
-        });
-      }
-      if (admin) {
-        post.id = req.auth.userId;
-      }
-      if (post.id !== req.auth.userId) {
-        return res.status(401).json({
-          error: new error("Requête non autorisée !"),
-        });
-      }
-      const filename = post.postFile.split("/images/")[1];
-      fs.unlink(`images/${filename}`, () => {
-        Posts.destroy({ where: { id: req.params.id } })
-          .then(() => res.status(200).json({ message: "Post supprimé !" }))
-          .catch((error) => res.status(400).json({ error }));
+  Posts.findOne({ where: { id: req.params.id } }).then((post) => {
+    if (!post) {
+      return res.status(404).json({
+        error: new error("Post non trouvé !"),
       });
-    })
-    .catch((error) => res.status(500).json({ error }));
+    }
+    // Compare userId avec le propriétaire du post pour supprimer
+    if (post.UserId !== req.auth.userId) {
+      return res.status(401).json({
+        error: new error("Requête non autorisée !"),
+      });
+    }
+    Posts.findOne({ where: { id: req.params.id } })
+      .then((post) => {
+        // Suppression de l'image dans le dossier images
+        const filename = post.postFile.split("/images/")[1];
+        fs.unlink(`images/${filename}`, () => {
+          Posts.destroy({ where: { id: req.params.id } })
+            .then(() => res.status(200).json({ message: "Post supprimé !" }))
+            .catch((error) => res.status(400).json({ error }));
+        });
+      })
+      .catch((error) => res.status(500).json({ error }));
+  });
 };
 
 exports.getOnePost = async (req, res, next) => {
@@ -83,7 +88,7 @@ exports.getOnePost = async (req, res, next) => {
   res
     .json(post)
     .then((post) => res.status(200).json(post))
-    .catch((error) => res.status(405).json({ error }));
+    .catch((error) => res.status(401).json({ error }));
 };
 
 exports.getAllPosts = async (req, res, next) => {
